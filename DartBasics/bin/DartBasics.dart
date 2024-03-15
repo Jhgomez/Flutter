e73354ffb2bf3ed5bc5049a8982d5e7709abd385
Juan Gomez <juan.e.g.f@outlook.com>
2024-03-14 22:45:32 -0600

    refactor: Fix package name

--- DartBasics/bin/DartBasics.dart
+++ DartBasics/bin/DartBasics.dart
@@ -1,4 +1,4 @@
-import 'package:DartBasics/DartBasics.dart' as dart_basics;
+import 'package:dart_basics/DartBasics.dart' as dart_basics;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_basics.calculate()}!');
