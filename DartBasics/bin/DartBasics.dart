506e3832e774e31a1a2edc798131785c93572396
Juan Gomez <juan.e.g.f@outlook.com>
2024-03-14 23:09:38 -0600

    chore: Declare variables

--- DartBasics/bin/DartBasics.dart
+++ DartBasics/bin/DartBasics.dart
@@ -2,4 +2,14 @@ import 'package:dart_basics/DartBasics.dart' as dart_basics;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_basics.calculate()}!');
+
+  // we can use var or specific type when declaring variables
+  // however when declaring a variable without value will make the variable
+  // of dynamic type
+  bool isTrue;
+  var isTrueTwo;
+  isTrue = false;
+  isTrue = true;
+  isTrueTwo = true;
+  isTrueTwo = "";
 }
